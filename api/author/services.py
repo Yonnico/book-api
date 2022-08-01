fd0b42d8261f2bcb493aebc2a9704ca835a063aa
YoungMorzh <pancake.00@mail.ru>
2022-08-01 16:15:19 +0300

    refactoring author

--- api/author/services.py
+++ api/author/services.py
@@ -1,6 +1,12 @@
-from flask import jsonify
+from flask import request, abort
 from api.author.db import all_authors
 from api.book.db import all_books
+
+
+def find_author_by_id(author_id):
+    author = list(filter(lambda a: a['id'] == author_id, all_authors))
+    return author
+
 
 def add_books_to_author(author):
     books = list(filter(lambda b: b['author_id'] == author['id'], all_books))
@@ -8,9 +14,13 @@ def add_books_to_author(author):
     author_with_books['books'] = books
     return author_with_books
 
-def find_author_by_id(author_id):
-    author = list(filter(lambda a: a['id'] == author_id, all_authors))
-    return author
+
+def get_all_authors():
+    return all_authors
+
+
+def get_all_authors_with_books():
+    return list(map(add_books_to_author, all_authors))
 
 
 def is_author_id_exist(id):
@@ -22,8 +32,42 @@ def is_author_id_exist(id):
 
 
 def remove_books_with_author(author):
-    books = list(filter(lambda b: b['author_id'] == author['id'], all_books))
-    for book in books:
-        books.remove(book)
+    for book in all_books:
+        all_books.remove(book)
     all_authors.remove(author)
-    return jsonify({"result": True})
+    return
+
+
+def validate_author_id(val):
+    return isinstance(val, int) and is_author_id_exist(val)
+
+
+def validate_nickname(val):
+    return isinstance(val, str) and len(val)
+
+
+def validate_name(val):
+    return isinstance(val, str) and len(val)
+
+
+def validate_add_author(nickname, name):
+    if not request.json:
+        abort(400)
+    if 'nickname' not in request.json:
+        abort(400)
+    if 'name' not in request.json:
+        abort(400)
+    if 'nickname' in request.json:
+        if not validate_nickname(nickname):
+            abort(400)
+    if 'name' in request.json:
+        if not validate_name(name):
+            abort(400)
+    id = all_authors[-1]['id'] + 1
+    author = {
+        'id': id,
+        'nickname': nickname,
+        'name': name
+    }
+    all_authors.append(author)
+    return author
