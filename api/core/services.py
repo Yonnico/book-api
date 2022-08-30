8f7c5aad0ecf3bc8a87c6b0ef3a07d3396dcede3
YoungMorzh <pancake.00@mail.ru>
2022-08-30 17:28:16 +0300

    add validation

--- api/core/services.py
+++
@@ -1,9 +0,0 @@
-from flask import abort
-
-def validate_len(val):
-    if not len(val):
-        return abort(404)
-    return
-
-def validate_for_str(val):
-    return isinstance(val, str) and len(val)
---
+++ api/core/validation.py
@@ -0,0 +1,5 @@
+def is_str(val):
+    return isinstance(val, str)
+
+def is_int(val):
+    return isinstance(val, int)
