a63dc9f6302a63aab737113088085b3d0c70a633
YoungMorzh <pancake.00@mail.ru>
2022-09-16 17:25:08 +0300

    refactor and division inti VIEW/CONTROLLER func

--- api/book/services.py
+++ api/book/services.py
@@ -1,47 +1,11 @@
-from api.author.db import all_authors
 from api.book.db import all_books
+
+from api.author.services import get_all_authors, get_author_by_id
 
 from api.book.validation import validate_annotation, validate_title
 from api.author.validation import validate_author_id
 
-
-
-def get_book_by_id(book_id):
-    book = list(filter(lambda b: b['id'] == book_id, all_books))
-    if len(book):
-        return book[0]
-    return None
-
-
-def get_book_with_author(book):
-    author = list(filter(lambda a: a['id'] == book['author_id'], all_authors))
-    author = author[0]
-    book_with_author = book.copy()
-    book_with_author.pop('author_id')
-    book_with_author['author'] = author
-    return book_with_author
-
-
-def get_all_books():
-    return all_books
-
-
-def get_books_with_authors():
-    return list(map(get_book_with_author, all_books))
-
-
-def remove_book(book_id):
-    book = get_book_by_id(book_id)
-    if book != None:
-        return all_books.remove(book)
-    return False
-
-
-def validate_and_add_book(title, annotation, author_id):
-    if not private_validate_book(title, annotation, author_id, True):
-        return None
-    return private_add_book(title, annotation, author_id)
-
+#FOR CONTROLLER
 
 def private_validate_book(title, annotation, author_id, required):
     if required or title != None :
@@ -67,6 +31,95 @@ def private_add_book(title, annotation, author_id):
 
     all_books.append(book)
     return book
+
+
+def get_link_by_author(book):
+    authors = get_all_authors()
+    author = list(filter(lambda a: a['id'] == book['author_id'], authors))
+    return author
+
+
+def get_link_by_book(author):
+    book = list(filter(lambda b: b['author_id'] == author['id'], all_books))
+    return book
+
+
+def get_author_with_book(author):
+    books = list(filter(lambda b: b['author_id'] == author['id'], all_books))
+    author_with_books = author.copy()
+    author_with_books['books'] = books
+    return author_with_books
+
+
+def get_book_with_author(book):
+    author = get_link_by_author(book)
+    author = author[0]
+    book_with_author = book.copy()
+    book_with_author.pop('author_id')
+    book_with_author['author'] = author
+    return book_with_author
+
+#FOR VIEW
+
+def get_book_by_id(book_id):
+    book = list(filter(lambda b: b['id'] == book_id, all_books))
+    if len(book):
+        return book[0]
+    return None
+
+
+def get_book_with_author_by_id(book_id):
+    book = get_book_by_id(book_id)
+    author = get_link_by_author(book)
+    book_with_author = book.copy()
+    book_with_author.pop('author_id')
+    book_with_author['author'] = author
+    return book_with_author
+
+
+def remove_author_with_books(author_id):
+    authors = get_all_authors()
+    author = get_author_by_id(author_id)
+    if author != None:
+        books = get_link_by_book(author)
+        for book in books:
+            all_books.remove(book)
+        return authors.remove(author)
+    return False
+
+
+def get_author_with_book_by_id(author_id):
+    author = get_author_by_id(author_id)
+    books = list(filter(lambda b: b['author_id'] == author['id'], all_books))
+    author_with_books = author.copy()
+    author_with_books['books'] = books
+    return author_with_books
+
+
+def get_authors_with_books():
+    authors = get_all_authors()
+    return list(map(get_author_with_book, authors))
+
+
+def get_all_books():
+    return all_books
+
+
+def get_books_with_authors():
+    return list(map(get_book_with_author, all_books))
+
+
+def remove_book(book_id):
+    book = get_book_by_id(book_id)
+    if book != None:
+        return all_books.remove(book)
+    return False
+
+
+def validate_and_add_book(title, annotation, author_id):
+    if not private_validate_book(title, annotation, author_id, True):
+        return None
+    return private_add_book(title, annotation, author_id)
 
 
 def validate_and_change_book(book_id, title, annotation, author_id):
